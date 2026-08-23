f5c2689b7770d980d8c99a6ae7db43e1b43e6ac9
juanda <juandaviddavila027@gmail.com>
2026-08-23 16:15:08 -0500

    Archivos base

---
+++ SlotMachine/SlotMachine.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class SlotMachine here.
+ * 
+ * @author Juan David Davila Ortiz,Juan Angel Orozco Lozano
+ * 
+ */
+public class SlotMachine
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SlotMachine
+     */
+    public SlotMachine()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
